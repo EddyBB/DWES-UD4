5a2ed2a9d7ca045a82b93ff4c8877fb775cc11a3
Eddy <eddy.gonzalez.chavez.al@iespoligonosur.org>
2021-10-21 12:32:55 +0200

    prueba conexion

--- ejercicio3.php
+++ ejercicio3.php
@@ -8,23 +8,23 @@
 </head>
 <body>
     <?php
-        $informacion = simplexml_load_file("./archivo.xml");
+        $file = simplexml_load_file("./archivo.xml");
         
-        /*printf("title: %s", $informacion->book[1]->title);
+        /*printf("title: %s", $file->book[1]->title);
         echo "<br>";
-        printf("genre: %s", $informacion->book[1]->genre);
+        printf("genre: %s", $file->book[1]->genre);
         echo "<br>";
-        printf("price: %s", $informacion->book[1]->price);
+        printf("price: %s", $file->book[1]->price);
         echo "<br>";
-        printf("author: %s", $informacion->book[1]->author);
+        printf("author: %s", $file->book[1]->author);
         echo "<br>";
-        printf("publish_date: %s", $informacion->book[1]->publish_date);
+        printf("publish_date: %s", $file->book[1]->publish_date);
         echo "<br>";
-        printf("decription: %s", $informacion->book[1]->description);
+        printf("decription: %s", $file->book[1]->description);
         echo "<br>";*/
-        echo "<table border = 1>";
+        /*echo "<table border = 1>";
         echo "<th>titulo</th> <th>genero</th> <th>precio</th>";
-        foreach ($informacion as $libro) {
+        foreach ($file as $libro) {
             //echo '<br>author: '.$libro->author.'<br>';
             echo "<tr>";
             echo '<td>'.$libro->title.'</td>';
@@ -35,7 +35,29 @@
             //echo 'publish_date: '.$libro->publish_date.'<br>';
             //echo "description: ".$libro->description."<br>\n";
             
+        }*/
+        
+        $milibro=$file->addChild("book");
+        $milibro->addAttribute("id","bk113");
+        $milibro->addChild("author","Kimia");
+        $milibro->addChild("title","Blood");
+        $milibro->addChild("genre","Horror");
+        $milibro->addChild("price","32.2");
+        $milibro->addChild("publish_date","2020-02-02");
+        $milibro->addChild("description","Here we have a horror movie.");
+        $file->asXML("archivo.xml");
+
+        echo "<table border = 1>";
+        echo "<th>titulo</th> <th>genero</th> <th>precio</th>";
+        foreach ($file -> book as $lib) {
+            echo "<tr>";
+            echo '<td>'.$lib->title.'</td>';
+            echo '<td>'.$lib->genre.'</td>';
+            echo '<td>'.$lib->price.'</td>';
+            echo "</tr>"; 
         }
+
+        
     ?>
 </body>
 </html>
